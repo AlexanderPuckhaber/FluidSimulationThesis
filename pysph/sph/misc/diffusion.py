872abb94f6523041ef07ca6dc303d290eb2c6951
AlexanderPuckhaber <alexpuckhaber@gmail.com>
2017-09-17 22:01:06 -0400

    revert revert

--- pysph/sph/misc/diffusion.py
+++ pysph/sph/misc/diffusion.py
@@ -27,8 +27,8 @@ class DiffuseMaterial(Equation):
     
     def loop(self, d_idx, s_idx, d_material_velocity, s_material_velocity, d_material_amount, s_material_amount, WIJ):
         material_gradient = (s_material_amount[s_idx] - d_material_amount[d_idx])
-        d_material_velocity[d_idx] += ( material_gradient * self.diffusion_speed ) #* WIJ
-        s_material_velocity[s_idx] -= ( material_gradient * self.diffusion_speed ) #* WIJ
+        d_material_velocity[d_idx] += ( material_gradient * self.diffusion_speed ) * WIJ
+        s_material_velocity[s_idx] -= ( material_gradient * self.diffusion_speed ) * WIJ
 
     def post_loop(self, d_idx, d_material_amount, d_material_velocity, dt, t):
         d_material_amount[d_idx] += d_material_velocity[d_idx] * dt

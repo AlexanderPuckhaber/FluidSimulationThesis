0b7a56b0e00fb7e06b2d8c24c467eb4ef4d735ae
AlexanderPuckhaber <alexpuckhaber@gmail.com>
2017-10-07 21:20:04 -0400

    set water density to rho0 * 1.01

--- pysph/examples/hydrostatic_tank_mixing.py
+++ pysph/examples/hydrostatic_tank_mixing.py
@@ -185,9 +185,9 @@ class HydrostaticTankMaterials(Application):
         # oil.copy_over_properties(materials.generateFullParticleProperties())
         
         ##### INITIALIZE PARTICLE PROPS #####
-        water.rho[:] = rho0 * 1.0
-        water.rho0[:] = rho0 * 1.0
-        waterRho = rho0 * 1.0
+        water.rho[:] = rho0 * 1.01
+        water.rho0[:] = rho0 * 1.01
+        waterRho = rho0 * 1.01
         oil.rho[:] = rho0
         oil.rho0[:] = rho0
         oilRho = rho0
